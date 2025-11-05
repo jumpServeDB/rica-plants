b82db11c1141f17677ae0b2cba468b50eacf362e
DBalint88 <DBalint88@gmail.com>
2025-11-05 13:48:54 +0000

    Set environment variables, set up gitignore

--- src/pages/auth/FormContainer.jsx
+++ src/pages/auth/FormContainer.jsx
@@ -1,18 +1,27 @@
 const FormContainer = (props) => {
   const { children } = props;
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col items-center mx-2 my-8">
+    <div className="flex">
+      <div className="relative hidden md:flex">
         <img
-          src="https://static-task-assets.react-formula.com/capstone_logo_dark.png"
-          alt=""
-          className="w-16 mb-2"
+          src="https://static-task-assets.react-formula.com/capstone_sign_in_scene.png"
+          className=" h-screen object-cover"
         />
-        <div className="font-playfair text-emerald-700 text-3xl">
-          Rica's Plants
-        </div>
+        <div className="absolute top-0 left-0 w-full h-full bg-black/50"></div>
       </div>
-      {children}
+      <div className="flex flex-col items-center justify-center h-screen bg-green-200 flex-1">
+        <div className="flex flex-col items-center mx-2 my-8">
+          <img
+            src="https://static-task-assets.react-formula.com/capstone_logo_dark.png"
+            alt=""
+            className="w-16 mb-2 "
+          />
+          <div className="font-playfair text-emerald-700 text-3xl">
+            Rica's Plants
+          </div>
+        </div>
+        {children}
+      </div>
     </div>
   );
 };
